09e03dc0e4df87854ed585e5a967f126babcb8c8
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-12-07 21:42:24 +0000

    add a+b button to set pos to 90

--- main.ts
+++ main.ts
@@ -1,6 +1,9 @@
 input.onButtonPressed(Button.A, function () {
     servos.P2.setAngle(180)
 })
+input.onButtonPressed(Button.AB, function () {
+    servos.P2.setAngle(90)
+})
 input.onButtonPressed(Button.B, function () {
     servos.P2.setAngle(0)
 })
