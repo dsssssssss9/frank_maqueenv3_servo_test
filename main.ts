edfad6c2113e8786a5b5ef30743febf5b7453cb9
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-12-07 21:45:33 +0000

    add basic motor test using extension

--- main.ts
+++ main.ts
@@ -1,13 +1,16 @@
 input.onButtonPressed(Button.A, function () {
     servos.P2.setAngle(180)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 73)
 })
 input.onButtonPressed(Button.AB, function () {
     servos.P2.setAngle(90)
+    maqueen.motorStop(maqueen.Motors.All)
 })
 input.onButtonPressed(Button.B, function () {
     servos.P2.setAngle(0)
 })
-basic.showIcon(IconNames.Yes)
+basic.showIcon(IconNames.Happy)
+maqueen.motorStop(maqueen.Motors.All)
 servos.P2.setAngle(90)
 basic.forever(function () {
 	
