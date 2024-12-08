a1c1dedc341bf18a02076f21e7024515611ff64d
dsssssssss9 <30810847+dsssssssss9@users.noreply.github.com>
2024-12-08 19:03:40 +0000

    add pause between servo & motor seems to work

--- main.ts
+++ main.ts
@@ -1,17 +1,29 @@
 input.onButtonPressed(Button.A, function () {
-    servos.P2.setAngle(180)
-    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 73)
+    servos.P1.setAngle(180)
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(1000)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 167)
 })
 input.onButtonPressed(Button.AB, function () {
-    servos.P2.setAngle(90)
+    servos.P1.setAngle(90)
     maqueen.motorStop(maqueen.Motors.All)
 })
 input.onButtonPressed(Button.B, function () {
-    servos.P2.setAngle(0)
+    servos.P1.setAngle(0)
+    maqueen.motorStop(maqueen.Motors.All)
+    basic.pause(1000)
+    maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 106)
 })
-basic.showIcon(IconNames.Happy)
+basic.showLeds(`
+    . # # # .
+    . . # . .
+    . . # . .
+    # . # . .
+    . # . . .
+    `)
 maqueen.motorStop(maqueen.Motors.All)
-servos.P2.setAngle(90)
+basic.pause(1000)
+servos.P1.setAngle(90)
 basic.forever(function () {
 	
 })
